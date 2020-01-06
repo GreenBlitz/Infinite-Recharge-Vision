eac8f82e0e0f9d94ef398b80f3208a8766bfc92a
TheOnion313 <royn77777@gmail.com>
2020-01-06 19:44:43 +0200

    nothing special really

--- examples/spliced_polygon.py
+++ examples/spliced_polygon.py
@@ -4,8 +4,8 @@ import numpy as np
 from constants import OUTER_PORT_TRIANGLE
 from constants.distances import OUTER_PORT_WIDTH
 
-thr = gbv.ColorThreshold([[34, 114], [215, 255], [72, 152]], gbv.ColorThreshold.THRESH_TYPE_HSV) + gbv.Erode(
-    3) + gbv.Dilate(15)
+thr = gbv.ColorThreshold([[34, 114], [215, 255], [72, 152]], 'HSV') + gbv.Erode(
+    3) + gbv.Dilate(10)
 
 tri = gbv.GameObject(OUTER_PORT_TRIANGLE)
 
@@ -25,8 +25,8 @@ def find_triangles(cnt):
 def main():
     finder = gbv.PolygonFinder(threshold_func=thr, game_object=tri)
     camera = gbv.AsyncUSBCamera(1)
-    camera.wait_start_reading()
     camera.set_exposure(-13)
+    camera.wait_start_reading()
     func = (thr + gbv.find_contours + gbv.sort_contours + gbv.contours_to_polygons + (
         lambda x: x[0] if len(x) > 0 else None) + find_triangles)
     draw_func = lambda frame: gbv.draw_contours(frame, func(frame), (0, 255, 0), thickness=5)
@@ -34,15 +34,15 @@ def main():
     window = gbv.CameraWindow('feed', camera, drawing_pipeline=func)
 
     thresh_window.show_async()
-    window.show_async()
+#    window.show_async()
 
     while True:
         val = func(camera.read()[1])
-        if len(val) != 2:
-            continue
-        loc1, loc2 = finder.locations_from_shapes(val, camera)
-        dis = np.abs(loc1[0] - loc2[0]) + 0.3
-        a = -np.arccos(dis / OUTER_PORT_WIDTH)
+        if len(val) == 2:
+            loc1, loc2 = finder.locations_from_shapes(shapes=val, camera=camera)
+            dis = np.abs(loc1[0] - loc2[0]) + 0.3
+            a = -np.arccos(dis / OUTER_PORT_WIDTH)
+            print(a)
 
 
 if __name__ == '__main__':
