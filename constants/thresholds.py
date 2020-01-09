7c4328806dbf40f0b9e97e6aae402f24f26773d8
idoheinemann <idohaineman@gmail.com>
2020-01-09 20:45:07 +0200

    found effective threshold for power cell detection(very effective (: )

--- constants/thresholds.py
+++ constants/thresholds.py
@@ -2,4 +2,4 @@ import gbvision as gbv
 
 VISION_TARGET_THRESHOLD = gbv.ColorThreshold([[0, 50], [100, 255], [0, 50]], gbv.ColorThreshold.THRESH_TYPE_BGR)
 OUTER_PORT_THRESHOLD = gbv.ColorThreshold([[0, 167], [155, 255], [58, 138]], gbv.ColorThreshold.THRESH_TYPE_HSV) + gbv.MedianBlur(3) + gbv.Dilate(2)
-POWER_CELL_THRESHOLD = gbv.ColorThreshold([[10, 50], [140, 220], [113, 193]], gbv.ColorThreshold.THRESH_TYPE_HSV) + gbv.MedianBlur(15) + gbv.Dilate(2)
+POWER_CELL_THRESHOLD = gbv.ColorThreshold([[24, 34], [201, 255], [25, 125]], 'HSV') + gbv.Erode(5) + gbv.Dilate(5)
