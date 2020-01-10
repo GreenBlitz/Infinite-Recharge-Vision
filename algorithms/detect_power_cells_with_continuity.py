f54fa31ab37cebbafe1e67805682588a246ab082
idoheinemann <idohaineman@gmail.com>
2020-01-10 15:07:58 +0200

    continuity integrated and tested, threshold is bad so that is left but basically done

--- algorithms/detect_power_cells_with_continuity.py
+++ algorithms/detect_power_cells_with_continuity.py
@@ -4,11 +4,12 @@ from constants import CONTOUR_MIN_AREA, SHAPE_LIFESPAN
 from constants.game_objects import POWER_CELL
 from constants.thresholds import POWER_CELL_THRESHOLD
 from .base_algorithm import BaseAlgorithm
+from exceptions.algorithm_incomplete import AlgorithmIncomplete
 
 
-def get_closest(shapes, finder):
+def get_closest(shapes, finder, camera):
     closest_id = None
-    distance = lambda x: gbv.distance_from_object(finder.locations_from_shapes([shapes[x]])[0])
+    distance = lambda x: gbv.distance_from_object(finder.locations_from_shapes([shapes.get(x)], camera)[0])
     for i in shapes:
         if closest_id is None or distance(i) < distance(closest_id):
             closest_id = i
@@ -24,13 +25,13 @@ class FindPowerCells(BaseAlgorithm):
 
         self.finder = gbv.CircleFinder(game_object=POWER_CELL, threshold_func=POWER_CELL_THRESHOLD,
                                        contour_min_area=CONTOUR_MIN_AREA)
+        self.shaper = self.finder.find_shapes
         self.debug = True
         if self.debug:
-            self.window = gbv.FeedWindow('feed', drawing_pipeline=
-            lambda x: (self.finder.find_shapes(x), x) + (
-                lambda x: gbv.draw_circles(frame=x[1], circs=x[0], color=(255, 0, 0))))
+            self.threshold = gbv.FeedWindow('threshold', drawing_pipeline=POWER_CELL_THRESHOLD)
+            self.window = gbv.FeedWindow('feed', drawing_pipeline=gbv.EMPTY_PIPELINE)
 
-        self.continues = gbv.ContinuesShapeWrapper(finding_pipeline=self.finder, frame=None, shapes=[],
+        self.continues = gbv.ContinuesShapeWrapper(finding_pipeline=self.shaper, frame=None, shapes=[],
                                                    shape_type='CIRCLE', shape_lifespan=SHAPE_LIFESPAN, track_new=True,
                                                    tracker_type='MEDIANFLOW')
         self.found_cell = False
@@ -39,14 +40,23 @@ class FindPowerCells(BaseAlgorithm):
     def _process(self, frame: gbv.Frame, camera: gbv.Camera):
         power_cells = self.continues.find_shapes(frame)
         if (self.closest_id == -1 and len(power_cells) > 0) or not power_cells.__contains__(self.closest_id):
-            self.closest_id = get_closest(power_cells, self.finder)
+            self.closest_id = get_closest(power_cells, self.finder, camera)
+        elif len(power_cells) == 0:
+            raise AlgorithmIncomplete
 
+        marked = frame
         if self.debug:
-            not_closest = self.continues.get_shapes_as_list()
-            not_closest.remove(power_cells[self.closest_id])
-            marked = gbv.PipeLine(lambda x: gbv.draw_circles(circs=not_closest, frame=x, color=(255, 0, 0)) + (
-                lambda x: gbv.draw_circles(circs=power_cells[self.closest_id], color=(0, 0, 255), frame=x)))(frame)
+            self.threshold.show_frame(frame)
+            if len(power_cells) > 0:
+                power_cells_list = self.continues.get_shapes_as_list()
+                marked = (gbv.PipeLine(
+                    (lambda x: gbv.draw_circles(circs=power_cells_list, frame=x, color=(255, 0, 0), thickness=5))) + gbv.PipeLine((
+                    lambda x: gbv.draw_circles(circs=[power_cells.get(self.closest_id)], color=(0, 0, 255), frame=x, thickness=5))))(
+                    frame)
             self.window.show_frame(marked)
+        if len(power_cells) == 0:
+            raise AlgorithmIncomplete
+        return self.finder.locations_from_shapes(shapes=[power_cells.get(self.closest_id)], camera=camera)[0]
 
     def reset(self, camera: gbv.Camera):
-        pass
+        camera.set_exposure(-5)
--- algorithms/find_hexagon.py
+++ algorithms/find_hexagon.py
@@ -1,4 +1,5 @@
 import gbvision as gbv
+import cv2
 
 from constants import OUTER_PORT, OUTER_PORT_THRESHOLD, CONTOUR_MIN_AREA
 from exceptions.algorithm_incomplete import AlgorithmIncomplete
@@ -12,7 +13,7 @@ class FindHexagon(BaseAlgorithm):
         BaseAlgorithm.__init__(self, output_key, error_key, conn, log_algorithm_incomplete)
         self.finder = gbv.ContourFinder(game_object=OUTER_PORT, threshold_func=OUTER_PORT_THRESHOLD,
                                         contour_min_area=200)
-        self.debug = False
+        self.debug = True
         self.window = gbv.FeedWindow('window')
 
     def _process(self, frame: gbv.Frame, camera: gbv.Camera):
@@ -23,7 +24,11 @@ class FindHexagon(BaseAlgorithm):
         :return: location and angle in reference to hexagon
         """
         if self.debug:
-            self.window.show_frame(frame)
+            k = self.window.last_key_pressed
+            if k != 'q':
+                self.window.show_frame(frame)
+            else:
+                cv2.destroyAllWindows()
         shapes = self.finder.find_shapes(frame)
         if len(shapes) == 0:
             raise AlgorithmIncomplete()
