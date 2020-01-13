3255dcef2d47617db3eaad2b05628cf7b255d5b2
idoheinemann <idohaineman@gmail.com>
2020-01-13 21:50:53 +0200

    configured rpi, can send stream and recieve

--- examples/recieve_tcp_stream.py
+++ examples/recieve_tcp_stream.py
@@ -7,5 +7,6 @@ def main():
     window.show()
 
 
+
 if __name__ == '__main__':
     main()
--- examples/send_tcp_stream.py
+++ examples/send_tcp_stream.py
@@ -1,9 +1,11 @@
 import gbvision as gbv
+import gbrpi as gbr
 
 
 def main():
     broadcaster = gbv.TCPStreamBroadcaster(5809)
     camera = gbv.USBStreamCamera(broadcaster, 0, gbv.UNKNOWN_CAMERA)
+    camera.set_exposure(0.5)
     camera.toggle_stream(True)
     while True:
         camera.read()
