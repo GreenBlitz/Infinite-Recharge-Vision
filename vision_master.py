683133019842ff44ed8db629d2ae951d24cc72f1
TheOnion313 <royn77777@gmail.com>
2020-01-15 10:27:27 +0200

    added led ring to vision_master

--- vision_master.py
+++ vision_master.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from algorithms import BaseAlgorithm
 
+LED_RING_PORT = 2
 TABLE_NAME = 'vision'
 TABLE_IP = '10.45.90.2'
 OUTPUT_KEY = 'output'
@@ -20,6 +21,8 @@ def main():
     logger.debug('initialized conn')
     camera = gbv.USBCamera(0, gbv.LIFECAM_3000)
     camera.rescale(0.5)
+    led_ring = gbrpi.electronics.led_ring.LedRing(LED_RING_PORT)
+
     logger.debug('initialized camera')
 
     conn.set('algorithm', 'hexagon')
@@ -38,7 +41,7 @@ def main():
                 logger.warning(f'Unknown algorithm type: {algo_type}')
             if algo_type != current_algo:
                 print(f'switched to: {algo_type}')
-                possible_algos[algo_type].reset(camera)
+                possible_algos[algo_type].reset(camera, led_ring)
             algo = possible_algos[algo_type]
             algo(frame, camera)
         current_algo = algo_type
