8980ec920183e4f09fed5829099b23ec376f65ec
idoheinemann <idohaineman@gmail.com>
2020-01-26 20:08:16 +0200

    added do_vision

--- constants/net.py
+++ constants/net.py
@@ -2,5 +2,5 @@ from tools.system import is_on_roborio_network
 
 __is_on_roborio_network = is_on_roborio_network()
 
-TCP_STREAM_IP = '10.45.90.8' if __is_on_roborio_network else '192.168.1.8'
+TCP_STREAM_IP = '10.45.90.76' if __is_on_roborio_network else '192.168.1.8'
 TCP_STREAM_PORT = 5809
--- upload.sh
+++ upload.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2046
 scp -r $(pwd) pi@frcvision.local:~/vision
