a5b68cbe0334e1e618d8cd5ab716934afbf44cdd
Ofek Kfir <ofek.kfir@gmail.com>
2020-01-29 15:16:46 +0200

    updated upload.sh with excution of rmvision.sh on pi first

--- upload.sh
+++ upload.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2046
+echo "You'll need to enter pi password twice, it's normal"
+
+ssh pi@frcvision.local ./rmvision.sh
 scp -r $(pwd) pi@frcvision.local:~/vision
+
